43c36c03a63e338fa2462fa7e2b93e5265982571
Daniel Nicodemo <dan.nicodemo@gmail.com>
2015-10-29 16:18:50 -0400

    migrations migrated, fixed missing ids

--- db/migrate/20151029201059_create_comments.rb
+++ db/migrate/20151029201059_create_comments.rb
@@ -1,6 +1,8 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.integer :user_id, null: false
+      t.integer :post_id, null: false
       t.text :body, null: false
       t.datetime :updated_at, null: false
 
