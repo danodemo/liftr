262239905dbcfdb0ba3cea9fde8f1034194ec380
Daniel Nicodemo <dan.nicodemo@gmail.com>
2015-10-29 15:56:22 -0400

    user model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+has_many :posts
+has_many :comments
+
+  validates_presence_of :username, :email, :password
+  validates_length_of :password, minimum: 12
+  validates_uniqueness_of :username, :email
 end
