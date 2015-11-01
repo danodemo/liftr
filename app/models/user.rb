da22b3b0c4922e167b4459d451ee6113e0692e77
Brian Dunlap <briandunlap589@gmail.com>
2015-11-01 15:03:49 -0500

    Debug create method in comments controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ has_many :posts
 has_many :comments
 
   validates_presence_of :username, :email, :password
-  validates_length_of :password, minimum: 12
+  validates_length_of :password, minimum: 6
   validates_uniqueness_of :username, :email
 end
