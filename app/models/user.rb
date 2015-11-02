3893ada0bf7cc28f9bda1c7d2b09e97fb3c74642
Brian Dunlap <briandunlap589@gmail.com>
2015-11-02 14:55:54 -0500

    Add bcrypt to Gemfile and hash value user authentication in sessesions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
+has_secure_password
+
 has_many :posts
 has_many :comments
 
-  validates_presence_of :username, :email, :password
-  validates_length_of :password, minimum: 6
+  validates_presence_of :username, :email
   validates_uniqueness_of :username, :email
 end
