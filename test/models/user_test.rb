4d35027766352513e1dd67f275bc1aef81865a1d
Daniel Nicodemo <dan.nicodemo@gmail.com>
2015-11-11 10:07:36 -0500

    first passing tests added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  #test "user successfully added to db" do
+  #  assert true
+  #end
+  test "emails must be present" do #This tests that a blank field will not trigger a .save
+    user = User.new #this should create an empty model
+    refute user.save #this says "prove that user doesn't save when blank"
+    assert user.errors[:email].present? #"Check that it's because e-mail is blank"
+  end
+
+  test "users successfully save when fields are present" do
+    user = User.new(username: "kittykitty", email: "catbox@litter.com", password_digest: "meeowmeow")
+    assert user.save
+    assert user.errors.empty?
   end
 end
